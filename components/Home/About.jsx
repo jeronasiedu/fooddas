bf10632bf2c1042b3b8304bb2524939c7e59882e
Sam-Tech-Ideas <agyemangsamuel235@gmail.com>
2022-10-16 23:35:40 +0000

    update

--- components/Home/About.jsx
+++ components/Home/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
   <div class="container px-5 py-24 mx-auto flex flex-wrap">
     <div class="flex flex-wrap -mx-4 mt-auto mb-auto lg:w-1/2 sm:w-2/3 content-start sm:pr-10">
       <div class="w-full sm:p-4 px-4 mb-6">
-        <h1 class="title-font font-medium text-xl mb-2 text-gray-900">Moon   try offal truffaut</h1>
+        <h1 class="title-font font-medium text-xl mb-2 text-gray-900">Moon    offal truffaut</h1>
         <div class="leading-relaxed">Pour-over craft beer pug drinking vinegar live-edge gastropub, keytar neutra sustainable fingerstache kickstarter.</div>
       </div>
       <div class="p-4 sm:w-1/2 lg:w-1/4 w-1/2">
